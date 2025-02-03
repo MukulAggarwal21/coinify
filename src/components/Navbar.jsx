3101b78e784cdc5d04e1f41546db17838f94dda4
MUKUL <mukul.2226csit1115@kiet.edu>
2025-02-03 22:37:07 +0530

    Minor Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,51 +18,56 @@ import {
   CNavLink,
 } from '@coreui/react'
 function Navbar() {
-    const [visible, setVisible] = useState(false)
+  const [visible, setVisible] = useState(false)
 
-    return (
-        <>
-         <CNavbar expand="lg" className=" bg-gray-800  h-20 text-white text-md">
-      <CContainer fluid>
-        <CNavbarBrand className=' text-white font-bold font-serif  text-9xl ml-10 pr-4' href="/"><h1>Coinify</h1></CNavbarBrand>
-        <CNavbarToggler onClick={() => setVisible(!visible)} />
-        <CCollapse className="navbar-collapse" visible={visible}>
-          <CNavbarNav className="me-auto">
-            <CNavItem  >
-              <CNavLink className=' text-white  hover:underline  mr-3' href="#" active>
-                Home
-              </CNavLink>
-            </CNavItem>
-            <CNavItem>
-              <CNavLink className=' text-white hover:underline  mr-3' href="#">Link</CNavLink>
-            </CNavItem>
-            <CDropdown variant="nav-item" popper={false}>
-              <CDropdownToggle className=' text-white hover:underline  mr-3' color="secondary">Dropdown button</CDropdownToggle>
-              <CDropdownMenu>
-                <CDropdownItem href="#">Action</CDropdownItem>
-                <CDropdownItem href="#">Another action</CDropdownItem>
-                <CDropdownDivider />
-                <CDropdownItem href="#">Something else here</CDropdownItem>
-              </CDropdownMenu>
-            </CDropdown>
-            <CNavItem>
-              <CNavLink href="#" className=' text-white hover:underline  mr-3'  disabled>
-                Disabled
-              </CNavLink>
-            </CNavItem>
-          </CNavbarNav>
-          <CForm className="d-flex">
-            <CFormInput type="search" className="me-2" placeholder="Search" />
-            <CButton type="submit" color="info" variant="outline">
-              Search
-            </CButton>
-          </CForm>
-        </CCollapse>
-      </CContainer>
-    </CNavbar>
+  return (
+    <>
+      <CNavbar expand="lg" className=" bg-gray-800  h-20 text-white text-md">
+        <CContainer fluid>
+          <CNavbarBrand className=' text-white font-bold font-serif  text-9xl ml-10 pr-4' href="/"><h1>Coinify</h1></CNavbarBrand>
+          <CNavbarToggler onClick={() => setVisible(!visible)} />
+          <CCollapse className="navbar-collapse" visible={visible}>
+            <CNavbarNav className="me-auto">
+              <CNavItem  >
+                <CNavLink className=' text-white  hover:underline  mr-3' href="#" active>
+                  Home
+                </CNavLink>
+              </CNavItem>
+              <CNavItem>
+                <CNavLink className=' text-white hover:underline  mr-3' href="#">Link</CNavLink>
+              </CNavItem>
+              <CDropdown variant="nav-item" popper={false}>
+                <CDropdownToggle className=' text-white hover:underline  mr-3' color="secondary">Coins</CDropdownToggle>
+                <CDropdownMenu>
+                  <CDropdownItem href="#">All Categories</CDropdownItem>
+                  <CDropdownItem href="#">Showroom</CDropdownItem>
+                  <CDropdownItem href="#">Top Gainers</CDropdownItem>
+                  <CDropdownItem href="#">Top Loosers</CDropdownItem>
+                  <CDropdownItem href="#">Recently Added </CDropdownItem>
+                  <CDropdownItem href="#">Trending Tokens to Watch </CDropdownItem>
+                  <CDropdownItem href="#">Most Popular</CDropdownItem>
+                  <CDropdownItem href="#">Coin Launches</CDropdownItem>
 
-        </>
-    )
+                  </CDropdownMenu>
+              </CDropdown>
+              <CNavItem>
+                <CNavLink href="#" className=' text-white hover:underline  mr-3' disabled>
+                  Disabled
+                </CNavLink>
+              </CNavItem>
+            </CNavbarNav>
+            <CForm className="d-flex">
+              <CFormInput type="search" className="me-2" placeholder="Search" />
+              <CButton type="submit" color="info" variant="outline">
+                Search
+              </CButton>
+            </CForm>
+          </CCollapse>
+        </CContainer>
+      </CNavbar>
+
+    </>
+  )
 }
 
 export default Navbar
